23b68ffc27d817ad3129b5f24c710eb10ba31cfa
PhongLV-9x <le.van.phong195@gmail.com>
2025-11-06 11:54:59 +0700

    less2

--- src/BT_JAVA_LESS2/Bai1_less2.java
+++ src/BT_JAVA_LESS2/Bai1_less2.java
@@ -1,12 +1,17 @@
 package BT_JAVA_LESS2;
 
+import javax.xml.namespace.QName;
 import java.util.Scanner;
 
 public class Bai1_less2 {
     public static void main(String[] args) {
         Bai1_less2 livecode = new Bai1_less2();
         //livecode.ex1();
-        livecode.ex2();
+        //livecode.ex2();
+        //livecode.ex3();
+        //livecode.ex4();
+        //livecode.ex5();
+        livecode.ex6();
     }
 
     public void ex1() {
@@ -63,5 +68,65 @@ public class Bai1_less2 {
 
         System.out.println("Chu vi HCN la: " + chuVi);
         System.out.println("Dien tich HCN la: " + dienTich);
+    }
+
+    public void ex3() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so bat ky: ");
+        int number = sc.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("=> Day la so chan");
+        } else if (number % 2 != 0) {
+            System.out.println("=> Day la so le");
+        }
+        {
+        }
+    }
+
+    public void ex4() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so thu nhat: ");
+        int firstNum = sc.nextInt();
+        System.out.print("Nhap so thu hai: ");
+        int secondNum = sc.nextInt();
+        if (firstNum == secondNum) {
+            System.out.println("=> Hai so co gia tri bang nhau ");
+        } else if (firstNum > secondNum) {
+            System.out.println("=> So co gia tri lon nhat la: " + firstNum);
+        } else if (firstNum < secondNum) {
+            System.out.println("=> So co gia tri lon nhat la: " + secondNum);
+        }
+    }
+
+    public void ex5() {
+        int firstNum = 100;
+        int secondNum = 200;
+
+        firstNum = firstNum + secondNum;
+        secondNum = firstNum - secondNum;
+        firstNum = firstNum - secondNum;
+
+        System.out.println("So a sau khi hoan doi la: " + firstNum);
+        System.out.println("So b sau khi hoan doi la: " + secondNum);
+    }
+    public void ex6(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap thong tin ten: ");
+        String name = sc.nextLine();
+
+        System.out.println("Nhap thong tin tuoi: ");
+        int age = sc.nextInt();
+
+        System.out.println("Nhap chieu cao: ");
+        float high = sc.nextFloat();
+
+        System.out.println("Nhap gioi tinh (F/M): ");
+        char general = sc.next().charAt(0);
+
+        System.out.println("\n ----- Thong tin ca nhan ----- \n");
+        System.out.println("Ten: " + name);
+        System.out.println("Tuoi: " + age);
+        System.out.println("Chieu cao: " + high + "m");
+        System.out.println("Gioi tinh (M/F): " + general);
     }
 }
